85df6d35eb3c5dcb6a76ad6cf2240f20a9289e5f
Tim Dysinger <tim@dysinger.net>
2013-11-02 09:14:15 -1000

    cleanup

--- Main.hs
+++ Main.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE TemplateHaskell #-}
-
-module Main where
+{-# LANGUAGE TemplateHaskell             #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
 import Data.FileEmbed
 import Graphics.UI.Gtk
@@ -8,18 +7,18 @@ import Data.ByteString.UTF8
 
 main :: IO ()
 main = do
-  _ <- initGUI
+  initGUI
   builder <- builderNew
-  -- builderAddFromFile builder "gtk-hello.ui" -- for debugging
-  builderAddFromString builder $ toString $(embedFile "gtk-hello.ui")
-  mainWindow <- builderGetObject builder castToWindow "window1"
+--builderAddFromFile builder "gtk-hello.ui" -- use this for designing the UI
+  builderAddFromString builder $ toString $(embedFile "gtk-hello.ui") -- use this for deploy
+  mainWindow  <- builderGetObject builder castToWindow "window1"
   closeButton <- builderGetObject builder castToButton "button2"
-  _ <- onClicked closeButton (widgetDestroy mainWindow)
-  _ <- onDestroy mainWindow mainQuit
+  onClicked closeButton (widgetDestroy mainWindow)
+  onDestroy mainWindow mainQuit
   label       <- builderGetObject builder castToLabel "label1"
   entry       <- builderGetObject builder castToEntry "entry1"
   applyButton <- builderGetObject builder castToButton "button1"
-  _ <- onClicked applyButton $ do
+  onClicked applyButton $ do
     name <- get entry entryText
     set label [ labelText := "Hello " ++ name ]
   widgetShowAll mainWindow
